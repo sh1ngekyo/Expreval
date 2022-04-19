28bede573792468d4bd2f3c7ae46fd52f5b583e5
sh1ngekyo <repllimate@gmail.com>
2022-04-19 13:12:02 +0700

    Add example project

---
+++ Expreval.CLI.TruthTable/Functions/BooleanImplication.cs
@@ -0,0 +1,22 @@
+﻿using Expreval.Core.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expreval.CLI.TruthTable.Functions
+{
+    class BooleanImplication : IBinaryFunction<bool, bool, bool>
+    {
+        private BooleanImplication() { }
+
+        private static Lazy<IBinaryFunction<bool, bool, bool>> instanse => new Lazy<IBinaryFunction<bool, bool, bool>>(() => new BooleanImplication());
+
+        public static IBinaryFunction<bool, bool, bool> Instanse => instanse.Value;
+
+        public bool Call(bool left, bool right)
+            => (!left) | right;
+    }
+}
---
+++ Expreval.CLI.TruthTable/Functions/BooleanNot.cs
@@ -0,0 +1,22 @@
+﻿using Expreval.Core.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expreval.CLI.TruthTable.Functions
+{
+    public sealed class BooleanNot : IUnaryFunction<bool, bool>
+    {
+        private BooleanNot() { }
+
+        private static Lazy<IUnaryFunction<bool, bool>> instanse => new Lazy<IUnaryFunction<bool, bool>>(() => new BooleanNot());
+
+        public static IUnaryFunction<bool, bool> Instanse => instanse.Value;
+
+        public bool Call(bool var)
+            => !var;
+    }
+}
